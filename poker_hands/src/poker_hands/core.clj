c51bcbffb068ff0168862b4bae1403378fd7c7e8
James Courtois <jcourtois@thoughtworks.com>
2014-02-14 17:30:40 -0600

    implemented high card

--- poker_hands/src/poker_hands/core.clj
+++ poker_hands/src/poker_hands/core.clj
@@ -1,6 +1,27 @@
 (ns poker-hands.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def card-values
+  {"A" 14, "K" 13, "Q" 12, "J" 11}
+)
+
+(defn convert-card-value
+  [card]
+  (if (nil? (get card-values (apply str(drop-last card))))
+  	(Integer/parseInt (apply str(drop-last card)))
+  	(get card-values (apply str(drop-last card)))
+  )
+)
+
+(defn greater-card
+  [first-card second-card]
+  (if (pos? (compare (convert-card-value first-card) (convert-card-value second-card)) )
+  	first-card
+  	second-card
+  )
+)
+
+(defn get-high-card
+	[hand]
+	(reduce greater-card hand)
+)
+
--- poker_hands/test/poker_hands/core_test.clj
+++ poker_hands/test/poker_hands/core_test.clj
@@ -2,6 +2,37 @@
   (:require [clojure.test :refer :all]
             [poker-hands.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest greater-card-test
+  (testing "Given two numeric cards, greater-card returns the higher value"
+    (is (= (greater-card "9H" "2H") "9H"))
+  )
+
+  (testing "Given one non-numeric card to a numeric one, greater-card returns the non-numeric value"
+    (is (= (greater-card "AD" "2D") "AD"))
+  )
+
+  (testing "Given two non-numeric cards, greater-card returns the non-numeric value"
+   (is (= (greater-card "AD" "JD") "AD"))
+  )
+)
+
+(deftest convert-card-value-test
+	(testing "Given a non-numeric card, it converts to numeric value")
+	(is (= (convert-card-value "AD") 14))
+
+	(testing "Given a non-numeric card, it converts to numeric value")
+	(is (= (convert-card-value "KS") 13))
+
+	(testing "Given a numeric card, it converts to numeric value")
+	(is (= (convert-card-value "6C") 6))
+
+	(testing "Given the number ten, it converts to numeric value")
+	(is (= (convert-card-value "10H") 10))
+)
+
+(deftest high-card-test
+	(testing "Given a hand of 5 cards, it returns the highest card")
+	(is (= (get-high-card ["2D" "JC" "5S" "QH" "AD"]) "AD"))
+	
+)
+
